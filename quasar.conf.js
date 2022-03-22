8f04187140b0261789336bd35904b26af62124fb
clisiog <clisio@outlook.com>
2022-03-22 13:29:19 -0300

    primary color change

--- quasar.conf.js
+++ quasar.conf.js
@@ -150,7 +150,7 @@ module.exports = configure(function (ctx) {
         display: "standalone",
         orientation: "portrait",
         background_color: "#ffffff",
-        theme_color: "#027be3",
+        theme_color: "#62b84c",
         icons: [
           {
             src: "icons/icon-128x128.png",
